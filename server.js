8aa35fd1dc79d1aea167593da9eb1a86c1ad7f8c
Italo71 <102165012+italo71@users.noreply.github.com>
2023-05-17 19:33:57 -0300

    alteração da porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./config/express.js')();
 //const cors = require('cors');
 //const port = app.get('port');
-const port = 80;
+const port = 8080;
 //const db = require("../API_Rotina/config/db");
 // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
